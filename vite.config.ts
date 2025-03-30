547ac0f17ea850e8dbe8ad411d3201c5a0abee08
Michael Rogers <60171929+mlr77@users.noreply.github.com>
2025-03-30 02:58:40 -1000

    changed workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Crucial for deploying to the root of <org>.github.io
+  base: '/',
 });
